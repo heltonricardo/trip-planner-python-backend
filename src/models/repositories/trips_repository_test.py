110b560dfbefc055c39a3cdcd3b0648e0f876b65
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2024-07-23 22:39:33 -0300

    test: update trip status

--- src/models/repositories/trips_repository_test.py
+++ src/models/repositories/trips_repository_test.py
@@ -1,4 +1,5 @@
 import uuid
+import pytest
 from datetime import datetime, timedelta
 from .trips_repository import TripsRepository
 from src.models.settings.db_connection_handler import db_connection_handler
@@ -8,12 +9,11 @@ db_connection_handler.connect()
 trip_id = str(uuid.uuid4())
 
 
+@pytest.mark.skip(reason="Interaction with DB")
 def test_create_trip():
     conn = db_connection_handler.get_connection()
     trips_repository = TripsRepository(conn)
-
     date = datetime.strptime("01-08-2024", "%d-%m-%Y")
-
     trips_infos = {
         "id": trip_id,
         "destination": "Tokio",
@@ -22,14 +22,19 @@ def test_create_trip():
         "owner_name": "José das Coves",
         "owner_email": "jose@coves.com"
     }
-
     trips_repository.create_trip(trips_infos)
 
 
+@pytest.mark.skip(reason="Interaction with DB")
 def test_find_trip_by_id():
     conn = db_connection_handler.get_connection()
     trips_repository = TripsRepository(conn)
-
     trip = trips_repository.find_trip_by_id(trip_id)
-    print()
     print(trip)
+
+
+@pytest.mark.skip(reason="Interaction with DB")
+def test_update_trip_status():
+    conn = db_connection_handler.get_connection()
+    trips_repository = TripsRepository(conn)
+    trips_repository.update_trip_status(trip_id)
