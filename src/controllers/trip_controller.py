90d5e3aca86cf2cf4f53905512416390872ad3ed
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2024-08-21 22:20:16 -0300

    feat: endpoint to create a trip

---
+++ src/controllers/trip_controller.py
@@ -0,0 +1,27 @@
+import uuid
+
+
+class TripController:
+    def __init__(self, trips_repository, emails_repository) -> None:
+        self.__trips_repository = trips_repository
+        self.__emails_repository = emails_repository
+
+    def create(self, body) -> dict:
+        try:
+            trip_id = str(uuid.uuid4())
+            emails = body.get("emails") or []
+            trip_info = {"id": trip_id, **body}
+            self.__trips_repository.create_trip(trip_info)
+            for email in emails:
+                email_id = str(uuid.uuid4())
+                self.__emails_repository.registry_email({
+                    "id": email_id,
+                    "trip_id": trip_id,
+                    "email": email,
+                })
+            return {"body": {"id": trip_id}, "status_code": 201}
+        except Exception as exception:
+            return {
+                "body": {"error": "Bad Request", "message": str(exception)},
+                "status_code": 400,
+            }
--- src/main/routes/trips_routes.py
+++ src/main/routes/trips_routes.py
@@ -1,4 +1,8 @@
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, request
+from src.controllers.trip_controller import TripController
+from src.models.repositories.trips_repository import TripsRepository
+from src.models.repositories.emails_repository import EmailsRepository
+from src.models.settings.db_connection_handler import db_connection_handler
 
 
 trips_routes_bp = Blueprint("trip_routes", __name__)
@@ -6,4 +10,9 @@ trips_routes_bp = Blueprint("trip_routes", __name__)
 
 @trips_routes_bp.route("/trips", methods=["POST"])
 def create_trip():
-    return jsonify({"hello": "world"}), 200
+    conn = db_connection_handler.get_connection()
+    trips_repository = TripsRepository(conn)
+    emails_repository = EmailsRepository(conn)
+    controller = TripController(trips_repository, emails_repository)
+    response = controller.create(request.json)
+    return jsonify(response["body"]), response["status_code"]
